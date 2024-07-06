0058bc5c37a53be6901786f5a69560d0754e1437
Rafail Abdyushev <rafaelab@mail.ru>
2024-07-06 18:01:40 +0300

    one more coroutine

--- reqHeaders.py
+++ reqHeaders.py
@@ -1,11 +1,7 @@
-import re
 import asyncio
-import aiohttp
 from urllib.parse import urlparse
-import sys, os, shutil, time
-import git,os
+import sys, os
 from git import Repo
-from pathlib import Path
 
 async def createRepo(repo_url, dirName):
     local_dir = dirName
@@ -14,10 +10,12 @@ async def createRepo(repo_url, dirName):
     for entry in tree: 
         print(entry.hexsha)
 
-def createTemplate(dirName, i):
+async def createTemplate(dirName, i):
     basename = str(os.path.basename(dirName)) + str(i)
     basename = os.path.join(os.getcwd(), basename)
-    if os.path.exists(basename): return createTemplate(dirName, i+1)
+    if os.path.exists(basename): 
+        task1 = asyncio.create_task(createTemplate(dirName, i+1))
+        return await task1
     return basename
 
 async def startAsync(urls):
@@ -26,8 +24,9 @@ async def startAsync(urls):
         pr = urlparse(url)
         dirName = os.path.join(os.getcwd(), pr.hostname)
         if os.path.exists(dirName):
-            dirName = createTemplate(dirName, 1)
-            # shutil.rmtree(dirName)
+            task1 = asyncio.create_task(createTemplate(dirName, 1))
+            dirName = await task1
+            # dirName = createTemplate(dirName, 1)
             os.mkdir(dirName)
         else:
             os.mkdir(dirName)
